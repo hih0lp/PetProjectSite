6842edaf1916e24b2ce6317fc9e291683372a390
hih0lp <mrlibobo@yandex.ru>
2024-09-06 20:38:56 +0300

    Make a custom json converter

--- Modules/ConvertersModule/JSONConverter.cs
+++ Modules/ConvertersModule/JSONConverter.cs
@@ -4,22 +4,51 @@ using System.Text.Json.Serialization;
 
 namespace PetProjectC_NeuroWeb.Modules.Converters
 {
-    public class JSONConverter : JsonConverter <UserData> 
+    public class CustomJSONConverter : JsonConverter<UserData>
     {
-
-
-
-        public override void Write(Utf8JsonWriter writer, UserData value, JsonSerializerOptions options)
-        {
-            throw new NotImplementedException();
-        }
 
         public override UserData? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            throw new NotImplementedException();
+            var login = "Undefined";
+            var hashedPassword = "Undefined";
+            var salt = "Unknown";
+
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonTokenType.PropertyName)
+                {
+                    var propertyName = reader.GetString();
+                    reader.Read();
+
+                    switch (propertyName?.ToLower())
+                    {
+                        case "login":
+                            login = reader.GetString();
+                            break;
+
+                        case "hashedPassword":
+                            hashedPassword = reader.GetString();
+                            break;
+
+                        case "salt":
+                            salt = reader.GetString();
+                            break;
+                    }
+
+                }
+            }
+            return new UserData(login, salt, hashedPassword);
+
+
         }
 
-        
+        public override void Write(Utf8JsonWriter writer, UserData userData, JsonSerializerOptions options)
+        {
+            writer.WriteStartObject();
+            writer.WriteString("login", userData.Login);
+            writer.WriteString("salt", userData.Salt);
+            writer.WriteEndObject();
+        }
 
     }
 }
